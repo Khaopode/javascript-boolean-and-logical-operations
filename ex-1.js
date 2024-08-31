82af3bd73941cc1147e609be67e85c728fd4199b
Khaopode <cjintate@gmail.com>
2024-08-31 18:56:46 +0700

    bp5-ChanunyaJ

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
+let isAllow = true;
 
 // Start coding here
-
+hasCriminalBlacklist = false
+isAllow = isOver18 && !hasCriminalBlacklist ;
 console.log(isAllow);
+console.log(!isOver18 && !hasCriminalBlacklist);
+
--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,26 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = true;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = true;
 
-let hasPromotion;
+let hasPromotion = true;
 
 console.log(hasPromotion);
+
+let CaseOne = lastMonthPaidMoreThan4000 && isWeekday && !(hasBoughtProductFromITCategory && hasAttendedDiscountEvent);
+let CaseTwo = isPlatinum ;
+
+hasPromotion = CaseOne || CaseTwo ;
+
+let John = (lastMonthPaidMoreThan4000 && isWeekday && hasAttendedDiscountEvent) && !(hasBoughtProductFromITCategory && isPlatinum) ;
+
+console.log (hasPromotion) ;
+console.log(John);
+
+
+
+
